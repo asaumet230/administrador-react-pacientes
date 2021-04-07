5d800221809bffd6213ad229ff468b54bc729509
Andres Felipe Saumet <andressaumet@gmail.com>
2021-04-07 12:21:13 -0500

    cambios
    cambios en json package

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { Fragment, useState, useEffect } from "react";
+import Formulario from "./components/Formulario";
+import Cita from "./components/Cita";
 
 function App() {
+  //Almacenar todas las citas:
+  const [citas, guardarCitas] = useState(
+    JSON.parse(localStorage.getItem("citas"))
+  );
+
+  //UseEffect para realizar ciertas opreciones cunado el state cambia:
+  useEffect(() => {
+    localStorage.setItem("citas", JSON.stringify(citas));
+  }, [citas]);
+
+  //Función que tome las citas actuales y agrege la nueva:
+  const crearCita = (cita) => {
+    guardarCitas([...citas, cita]);
+  };
+
+  //Eliminar Cita:
+  const eliminarCita = (id) => {
+    const nuevaCita = citas.filter((cita) => cita.id !== id);
+    guardarCitas(nuevaCita);
+  };
+
+  //Mensaje de Inicio:
+  const titulo =
+    citas.length === 0 ? "Agrega Citas" : "Administra Citas";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <h1>Administrador de citas</h1>
+      <div className="container">
+        <div className="row">
+          <div className="one-half column">
+            <Formulario crearCita={crearCita} />
+          </div>
+          <div className="one-half column">
+            <h2>{titulo}</h2>
+            {citas.map((cita) => (
+              <Cita
+                key={cita.id}
+                cita={cita}
+                eliminarCita={eliminarCita}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+    </Fragment>
   );
 }
+
+
+
 
 export default App;
